e3102ebfa3c469dae72667cc0e5acc9783341892
Pop Alexandru <alexandrupp55@gmail.com>
2026-09-08 18:48:16 +0300

    Change learning rate scheduler to dynamic milestones
    Updated learning rate scheduler milestones to be dynamic based on epochs.

--- models/ResNet_34.py
+++ models/ResNet_34.py
@@ -214,8 +214,8 @@ class ResNet_34(nn.Module):
             
             crit = nn.CrossEntropyLoss()
             optimizer = torch.optim.SGD(self.parameters(), lr=0.05, momentum=0.9, weight_decay=5e-4)
-            # Learning rate downscaled 10x at epochs 10, 20, 25
-            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20, 25], gamma=0.1)
+            # Learning rate dynamic 10x downscaling
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[epochs*1/3, epochs*2/3, epochs*2.5/3], gamma=0.1)
             train_loss_history = []
             val_loss_history = []
             
