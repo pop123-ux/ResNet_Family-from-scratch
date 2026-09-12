f907de7898898ba11527b63b26e6d073091dec4b
Pop Alexandru <alexandrupp55@gmail.com>
2026-09-12 18:52:20 +0300

    Update ResNet_12.py

--- models/ResNet_12.py
+++ models/ResNet_12.py
@@ -112,7 +112,7 @@ class ResNet_12(nn.Module):
     """
     
     DEFAULT_WEIGHTS = (
-    os.path.join(os.path.dirname(os.path.abspath(__file__))),
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     "checkpoints",
     "ResNet12_cifar100.pth",
     )
